e4440f6131a42f50377b24fe0526225295bb7866
MlNl-PEKKA <punithg360@gmail.com>
2025-02-24 10:26:10 +0530

    chore: setup neon and drizzle

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,35 +2,21 @@
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
 import { sql } from "drizzle-orm";
-import {
-  index,
-  integer,
-  pgTableCreator,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { index, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `chirp-stuff_${name}`);
-
-export const posts = createTable(
-  "post",
+export const users = pgTable(
+  "user",
   {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
+    id: uuid("id").defaultRandom().primaryKey(),
+    name: varchar("name", { length: 256 }).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
+      () => sql`CURRENT_TIMESTAMP`,
     ),
   },
   (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+    idIndex: index("id_idx").on(example.id),
+  }),
 );
